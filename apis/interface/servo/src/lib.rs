372e94ba7eba3131fe349a30b2ea1c80d3d54bba
inesmaria08 <inesmaria.moraru@gmail.com>
2024-09-24 16:32:54 +0300

    unittests addition

--- apis/interface/servo/src/lib.rs
+++ apis/interface/servo/src/lib.rs
@@ -1,13 +1,12 @@
 #![no_std]
 
-use libtock_platform::{CommandReturn, ErrorCode, Syscalls};
-//use libtock_unittest::command_return::success;
+use libtock_platform::{ErrorCode, Syscalls};
 
 pub struct Servo<S: Syscalls>(S);
 
 impl<S: Syscalls> Servo<S> {
     pub fn servo_exists() -> Result<(), ErrorCode> {
-        let val = S::command(DRIVER_NUM, 0, 0, 0).is_success();
+        let val = S::command(DRIVER_NUM, SERVO_EXISTS, 0, 0).is_success();
         if val == true {
             Ok(())
         } else {
@@ -16,15 +15,15 @@ impl<S: Syscalls> Servo<S> {
         }
     }
     pub fn set_angle(index: u32, angle: u32) -> Result<(), ErrorCode> {
-        S::command(DRIVER_NUM, 1, index, angle).to_result()
+        S::command(DRIVER_NUM, SET_ANGLE, index, angle).to_result()
     }
-    pub fn get_angle(index: u32) -> Result<(), ErrorCode> {
-        S::command(DRIVER_NUM, 2, index, 0).to_result()
+    pub fn get_angle(index: u32) -> Result<u32, ErrorCode> {
+        S::command(DRIVER_NUM, GET_ANGLE, index, 0).to_result()
     }
 }
 
-// #[cfg(test)]
-// mod tests;
+#[cfg(test)]
+mod tests;
 
 // -----------------------------------------------------------------------------
 // Driver number and command IDs
@@ -33,4 +32,6 @@ impl<S: Syscalls> Servo<S> {
 const DRIVER_NUM: u32 = 0x90009;
 
 // Command IDs
-//const SET_ANGLE: u16 = 1;
+const SERVO_EXISTS: u32 = 0;
+const SET_ANGLE: u32 = 1;
+const GET_ANGLE: u32 = 2;
---
+++ apis/interface/servo/src/tests.rs
@@ -0,0 +1,32 @@
+use libtock_platform::ErrorCode;
+use libtock_unittest::fake;
+
+type Servo = super::Servo<fake::Syscalls>;
+
+#[test]
+fn no_driver() {
+    let _kernel = fake::Kernel::new();
+    assert_eq!(Servo::servo_exists(), Err(ErrorCode::Fail))
+}
+#[test]
+fn servo_exists() {
+    let kernel = fake::Kernel::new();
+    let driver = fake::Servo::<2>::new();
+    kernel.add_driver(&driver);
+    assert_eq!(Servo::servo_exists(), Ok(()));
+}
+#[test]
+fn set_angle() {
+    let kernel = fake::Kernel::new();
+    let driver = fake::Servo::<2>::new();
+    kernel.add_driver(&driver);
+    assert_eq!(Servo::set_angle(1, 90), Ok(()));
+}
+#[test]
+fn get_angle() {
+    let kernel = fake::Kernel::new();
+    let driver = fake::Servo::<2>::new();
+    kernel.add_driver(&driver);
+    assert_eq!(Servo::set_angle(1, 45), Ok(()));
+    assert_eq!(Servo::get_angle(1), Ok(45));
+}
